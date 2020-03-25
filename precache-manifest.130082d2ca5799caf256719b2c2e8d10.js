147fabb71d320274b13fd0c56939fc5ead2ac5eb
Joshua McCue <joshuamccue@Joshuas-MBP.fios-router.home>
2020-03-25 16:45:26 -0400

    Updates

---
+++ precache-manifest.130082d2ca5799caf256719b2c2e8d10.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985f0b7c5fc324e4824693c73cda9e2c",
+    "url": "/drumBoard/index.html"
+  },
+  {
+    "revision": "cb8d9c10808f7401be1a",
+    "url": "/drumBoard/static/css/main.34f05c81.chunk.css"
+  },
+  {
+    "revision": "c6120f786e53912c98c7",
+    "url": "/drumBoard/static/js/2.6ee66d48.chunk.js"
+  },
+  {
+    "revision": "108fb3bdf508c233ad8c482c42cea3cc",
+    "url": "/drumBoard/static/js/2.6ee66d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8d9c10808f7401be1a",
+    "url": "/drumBoard/static/js/main.45ee9c5e.chunk.js"
+  },
+  {
+    "revision": "1c3fb0bada9efc441b7b",
+    "url": "/drumBoard/static/js/runtime-main.89389e00.js"
+  },
+  {
+    "revision": "7658f906ff57a27347eeedf3db361a13",
+    "url": "/drumBoard/static/media/bell-1.7658f906.mp3"
+  },
+  {
+    "revision": "27592ab51d90adab537d1e392db3cb64",
+    "url": "/drumBoard/static/media/bell-2.27592ab5.mp3"
+  },
+  {
+    "revision": "05cb642301bce957cc1cd40b146d7baa",
+    "url": "/drumBoard/static/media/bell-3.05cb6423.mp3"
+  },
+  {
+    "revision": "faafa8b5062b874c0bf5f31113d299f5",
+    "url": "/drumBoard/static/media/piano-1.faafa8b5.mp3"
+  },
+  {
+    "revision": "c2f1538af6e76820f8df603e2e7cdac6",
+    "url": "/drumBoard/static/media/piano-2.c2f1538a.mp3"
+  },
+  {
+    "revision": "11b3ac561d53c845c9b37665e6ba10bc",
+    "url": "/drumBoard/static/media/piano-3.11b3ac56.mp3"
+  },
+  {
+    "revision": "241c9c880780ffbdb892e9b54e1a1155",
+    "url": "/drumBoard/static/media/piano-4.241c9c88.mp3"
+  },
+  {
+    "revision": "8f0e21a290d5ca9156cd98a21eb0b901",
+    "url": "/drumBoard/static/media/piano-5.8f0e21a2.mp3"
+  },
+  {
+    "revision": "4a6a12bbde1e9ad743aa0c298ce2493c",
+    "url": "/drumBoard/static/media/river.4a6a12bb.jpg"
+  },
+  {
+    "revision": "4679bba5b6d22e4f395cfaa5d1fb3b5b",
+    "url": "/drumBoard/static/media/soundA.4679bba5.wav"
+  },
+  {
+    "revision": "04c3225c7e9db85da822e3dab6ff6d45",
+    "url": "/drumBoard/static/media/soundB.04c3225c.wav"
+  },
+  {
+    "revision": "9632ac563dd900dc4bb85d7b49af239c",
+    "url": "/drumBoard/static/media/soundC.9632ac56.wav"
+  },
+  {
+    "revision": "9db9cfcafd6bd2b202b770fb641117d1",
+    "url": "/drumBoard/static/media/soundD.9db9cfca.wav"
+  },
+  {
+    "revision": "d24b097cfcc52a4a34e04e0f31fec6bb",
+    "url": "/drumBoard/static/media/soundE.d24b097c.wav"
+  },
+  {
+    "revision": "e62391268554b7233dd5151a898a4acf",
+    "url": "/drumBoard/static/media/soundF.e6239126.flac"
+  },
+  {
+    "revision": "fdc8edaae66246bc4f8df4dbdab9615d",
+    "url": "/drumBoard/static/media/soundG.fdc8edaa.wav"
+  },
+  {
+    "revision": "0deb6a9e3c1950121841df584453b6cc",
+    "url": "/drumBoard/static/media/soundH.0deb6a9e.wav"
+  },
+  {
+    "revision": "e4b0cdec2b011c9c6592c3e862771163",
+    "url": "/drumBoard/static/media/soundI.e4b0cdec.wav"
+  },
+  {
+    "revision": "9e85ad58a49fc64d81c9628ec23e3f20",
+    "url": "/drumBoard/static/media/three.9e85ad58.mp3"
+  }
+]);
